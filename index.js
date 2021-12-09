9547ed9b6da76a664b71fb82100ee70493d87c44
Alexander Sokolov <thorpe.nulled@gmail.com>
2021-12-09 17:38:27 +0300

    add animations for mobile version

--- index.js
+++ index.js
@@ -24,13 +24,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const data = await res.json();
     const template = data.map((el, index) => {
       let classBg = 'green';
-      if (data[index].level < 70){
+      if (data[index].level < 70) {
         classBg = data[index].level < 36 ? 'red' : 'yellow'
-      };
+      }
+      ;
 
       levels.push({
-        'id': data[index].id,
-        'level': data[index].level,
+          'id': data[index].id,
+          'level': data[index].level,
         }
       );
 
@@ -45,18 +46,15 @@ document.addEventListener('DOMContentLoaded', () => {
   })();
 
 
-
-
-
   (async () => {
     const res = await fetch('./projects/projects.json');
     const data = await res.json();
     const template = data.map((el, index) => {
       const side = data[index].num % 2 === 0 ? 'left' : 'right';
       return `<div class="projects__project ${side}">
-                <div class="projects__project-title"'">
+                <a href="${data[index].link}" class="projects__project-title" target="_blank">
                    ${data[index].title}
-                </div>
+                </a>
                 <div class="projects__project-container">
                   <a href="${data[index].link}" class="projects__project-img_link" target="_blank">
                      <img src="projects/img/${data[index].image}" 
@@ -65,6 +63,9 @@ document.addEventListener('DOMContentLoaded', () => {
                   <div class="projects__project-info">
                     <div class="projects__project-description">
                       <span class="projects__project-param"><strong>Description</strong>:</span> ${data[index].description}
+                    </div>
+                    <div class="projects__project-type">
+                      <span class="projects__project-param"><strong>App type</strong>:</span> ${data[index].type}
                     </div>
                     <div class="projects__project-stack">
                       <span class="projects__project-param"><strong>Stack</strong>:</span> ${data[index].stack}
@@ -132,8 +133,6 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 
-
-
   let isY800 = true;
 
   function scrollShowUp() {
@@ -149,70 +148,113 @@ document.addEventListener('DOMContentLoaded', () => {
 
     }
 
+    const leftArray = Array.from(projectFromLeft);
+    const rightArray = Array.from(projectFromRight);
+    const projectImg = Array.from(projectImgEl);
+
+    function loopIt(i, arr, direction, delay) {
+      setTimeout(function () {
+        if (direction === 'left') {
+          arr[i].classList.add('from-left');
+          arr[i].classList.remove('left');
+        } else if (direction === 'right') {
+          arr[i].classList.add('from-right');
+          arr[i].classList.remove('right');
+        }
+        if (i < arr.length - 1) loopIt(i + 1, arr, direction, delay + 2000)
+      }, delay);
+    }
+
+    function skillsAnimation() {
+      skillsEl.classList.add('from-left');
+      levels.forEach((el) => {
+        setTimeout(() => {
+          document.getElementById(`skill-${el.id}`).classList.add(`skills-${el.level}`);
+          document.getElementById(`skill-${el.id}`).classList.remove('start-skill');
+        }, 1500);
+      })
+    }
+
+    function projectsAnimation() {
+      isY800 = false;
+      loopIt(0, leftArray, 'left', 0);
+      loopIt(0, rightArray, 'right', 0);
+
+      projectImg.forEach((el) => {
+        el.addEventListener('mouseover', () => {
+          el.classList.add('active')
+        });
+        el.addEventListener('mouseout', () => {
+          el.classList.remove('active')
+        });
+      })
+    }
+
+    function rsSchoolAnimation(){
+      rsSchoolEl.classList.add('rotateRS');
+      setTimeout(() => rsSchoolEl.classList.remove('rotateRS'), 2000);
+    }
+
+    if (htmlWidth > 683) {
+
       if (y > 200) {
-        skillsEl.classList.add('from-left');
-
-        levels.forEach((el) => {
-          setTimeout(() => {
-            document.getElementById(`skill-${el.id}`).classList.add(`skills-${el.level}`);
-            document.getElementById(`skill-${el.id}`).classList.remove('start-skill');
-          }, 1500);
-        })
-
+        skillsAnimation();
       }
 
       if (y > 800 && isY800) {
-        isY800 = false;
-        const leftArray = Array.from(projectFromLeft);
-        const rightArray = Array.from(projectFromRight);
-
-        function loopIt(i, arr, direction, delay) {
-          setTimeout(function () {
-            if (direction === 'left') {
-              arr[i].classList.add('from-left');
-              arr[i].classList.remove('left');
-            }
-            else if (direction === 'right') {
-              arr[i].classList.add('from-right');
-              arr[i].classList.remove('right');
-            }
-            if (i < arr.length - 1) loopIt(i + 1, arr, direction, delay + 2000)
-          }, delay);
-        }
-
-        loopIt(0, leftArray, 'left', 0);
-        loopIt(0, rightArray, 'right', 0);
-
-        const projectImg = Array.from(projectImgEl);
-        projectImg.forEach((el) => {
-          el.addEventListener('mouseover', () => { el.classList.add('active') });
-          el.addEventListener('mouseout', () => { el.classList.remove('active') });
-        })
-
+        projectsAnimation();
       }
 
       if (y > 2500) {
         codeSampleEl.classList.add('from-scale');
       }
 
-      if (y > 3400) {
+      if (y > 3100) {
         educationEl.classList.add('appear-from-top');
       }
-      if (y > 3500) {
+      if (y > 3200) {
         languageEl.classList.add('appear-from-top');
       }
-      if (y > 3600) {
+      if (y > 3300) {
         contactsEl.classList.add('from-scale');
       }
-      if (y > 3800) {
-        rsSchoolEl.classList.add('rotateRS');
-        setTimeout(() => rsSchoolEl.classList.remove('rotateRS'), 2000);
+      if (y > 3600) {
+        rsSchoolAnimation();
       }
+
+    } else {
+
+      if (y > 500) {
+        skillsAnimation();
+      }
+
+      if (y > 1300 && isY800) {
+        projectsAnimation();
+      }
+
+      if (y > 3600) {
+        codeSampleEl.classList.add('from-scale');
+      }
+
+      if (y > 4100) {
+        educationEl.classList.add('appear-from-top');
+      }
+      if (y > 4300) {
+        languageEl.classList.add('appear-from-top');
+      }
+      if (y > 4500) {
+        contactsEl.classList.add('from-scale');
+      }
+      if (y > 4700) {
+        rsSchoolAnimation();
+      }
+
     }
 
-  rsSchoolEl.addEventListener('mouseover', () => rsSchoolEl.classList.add('rotateRS'))
-  rsSchoolEl.addEventListener('mouseout', () => rsSchoolEl.classList.remove('rotateRS'))
 
+    rsSchoolEl.addEventListener('mouseover', () => rsSchoolEl.classList.add('rotateRS'))
+    rsSchoolEl.addEventListener('mouseout', () => rsSchoolEl.classList.remove('rotateRS'))
 
+  }
 
 });
